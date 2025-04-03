ec094b0507410de28cee0ec8bd662bfd192b4616
Cristian1BD <cristian.bedoya1036@gmail.com>
2025-04-03 09:22:22 -0500

    Entidad

--- backend2/src/main/java/com/cesde/proyecto_integrador/ProyectoIntegradorApplication.java
+++ backend2/src/main/java/com/cesde/proyecto_integrador/ProyectoIntegradorApplication.java
@@ -2,12 +2,15 @@ package com.cesde.proyecto_integrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.cesde.proyecto_integrador.model")
+@EnableJpaRepositories(basePackages = "com.cesde.proyecto_integrador.repository")
 public class ProyectoIntegradorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProyectoIntegradorApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoIntegradorApplication.class, args);
+    }
 }
--- backend2/src/main/java/com/cesde/proyecto_integrador/repository/UserRepository.java
+++ backend2/src/main/java/com/cesde/proyecto_integrador/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.cesde.proyecto_integrador.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cesde.proyecto_integrador.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     // Removed as it is already inherited from JpaRepository with proper null constraints
     User findByEmail(String email);
